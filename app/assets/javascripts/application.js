5ee9c13f5d7f9ba432abf9eb144e13f3811e92ad
ikona23@gmail.com <ikona23@gmail.com>
2015-03-02 01:30:52 +1300

    catering_db

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -124,27 +124,66 @@ $(document).ready(function() {
 
 
 $(document).ready(function() {
-    var mapCanvas = document.getElementById('map-canvas');
-    var myLatLng = new google.maps.LatLng(48.141528, 17.110242);
-    var mapOptions = {
-        zoom: 13,
-        center: myLatLng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    var map;
+    var markers = [];
 
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+    function initialize() {
+        var myLatLng = new google.maps.LatLng(48.141528, 17.110242);
+        var mapOptions = {
+            zoom: 12,
+            center: myLatLng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
 
-    var infowindow = new google.maps.InfoWindow({
-        content: 'Regal Burger',
-        position: myLatLng
-    });
-    infowindow.open(map);
+        // This event listener will call addMarker() when the map is clicked.
+        google.maps.event.addListener(map, 'click', function(event) {
+            addMarker(event.latLng);
+        });
 
-    var infwindow = new google.maps.InfoWindow({
-        content: 'Regal Burger' +
-        ' Palackeho 4',
-        position: buLatLng
-    });
-    infowindow.open(map);
+        // Adds a marker at the center of the map.
+        addMarker(myLatLng);
+    }
+
+// Add a marker to the map and push to the array.
+    function addMarker(location) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            labelContent: "$425K",
+            labelAnchor: new google.maps.Point(22, 0),
+            labelClass: "labels", // the CSS class for the label
+            labelStyle: {opacity: 0.75},
+            title: 'Regal Burger' +
+            '-Palackého 4'
+        });
+        markers.push(marker);
+    }
+
+// Sets the map on all markers in the array.
+    function setAllMap(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+// Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+        setAllMap(null);
+    }
+
+// Shows any markers currently in the array.
+    function showMarkers() {
+        setAllMap(map);
+    }
+
+// Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 });
-
